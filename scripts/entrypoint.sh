906f3b52a99c1a1dd68b4867e6f7f65a8f73631c
iromli <isman.firmansyah@gmail.com>
2020-01-24 01:23:26 +0700

    update license

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -e
 
-run_entrypoint() {
-    touch /deploy/touched
-}
-
-cat << LICENSE_ACK
-
-# ================================================================================= #
-# Gluu License Agreement: https://www.gluu.org/support-license/                     #
-# The use of Gluu Server Enterprise Edition is subject to the Gluu Support License. #
-# ================================================================================= #
-
-LICENSE_ACK
-
 if [ -f /etc/redhat-release ]; then
     source scl_source enable python27 && python /app/scripts/wait.py
     source scl_source enable python27 && python /app/scripts/entrypoint.py
